5fa89751f23b7ea92d95487ffa36e1e24d83957d
koheinishimura66 <t13551kn@sfc.keio.ac.jp>
2014-07-19 08:56:36 +0900

    軌道修正

--- js/app.js
+++ js/app.js
@@ -1,26 +1,7 @@
 var table = {"チキンおろしだれ":[2.7,1.5,1.2,458,1.4],"ラーメン":[2,3,4,300,1.0]}
-console.log("hello world")
-/*var getFoodList = function(){
-  var checkbox = document.querySelectorAll("[name=menu]");
-  var i = 0;
-  var result = [];
-  while(i < checkbox.length){
-    if(checkbox[i].checked){
-      result.push(checkbox[i].value);
-    }
-    i = i + 1;
-  }
-  return result;
-};
-var foods = getFoodList();
-var a = [0, 0, 0, 0, 0,]
-var n = 0
-while(n < getFoodList.length)
-  a += table[getFoodList[n]]
-  n += 1
-test() = return a
+var getFoodList = function(){
 
-var T = [a[0]/6*5, a[1]/3*5, a[2]/16*5]
+var T = table["チキンおろしだれ"]
 
 window.onload = function() {
   var rc = new html5jp.graph.radar("sample");
