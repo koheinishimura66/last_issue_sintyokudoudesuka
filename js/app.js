4c9a28b2e679ad4d50092e6c54add92c8df41da3
koheinishimura66 <t13551kn@sfc.keio.ac.jp>
2014-07-19 11:25:52 +0900

    Revert "チェックボックス迷走中"
    This reverts commit edd616dad807d61adb79a2491bf548ac59dde946.
    
    Conflicts:
    	index.html
    	js/app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
+<<<<<<< HEAD
 var table = {"チキンおろしだれ":[2.7,1.5,1.2,458,1.4],"ラーメン":[2,3,4,300,1.0]}
 var getFoodList = function(){
 
 var T = table["チキンおろしだれ"]
 
+=======
+>>>>>>> parent of edd616d... チェックボックス迷走中
 window.onload = function() {
   var rc = new html5jp.graph.radar("sample");
   if( ! rc ) { return; }
   var items = [
     ["前回のあなた", 5, 3, 5],
-    ["あなたの値", T[0], T[1], T[2]]
+    ["あなたの値", 3, 4, 3]
   ];
   var params = {
     aCap: ["赤", "緑", "黄"]
